6698c6a5357ae66a5620b6dd06a2a975886f9e9b
rpatetti <patetti.rafael@gmail.com>
2024-04-20 16:52:10 -0300

    finalizando projeto landingpage

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
-console.log("olá mundo");
+AOS.init();
+
+const dataDoEvento = new Date("apr 19, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / (1000 * 60 * 60 * 24))
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutosAteOEvento = Math.floor(distanciaAteOEvento %  (1000 * 60 * 60) / (1000 * 60))
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % 1000 * 60) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000);
